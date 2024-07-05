281998bbddd5fe0280603b86cfc58e53af7d720f
guvely <natureingu@naver.com>
2024-07-05 10:52:29 +0900

    'first'

---
+++ src/main/java/com/example/xy/XyApplication.java
@@ -0,0 +1,13 @@
+package com.example.xy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/xy/XyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
